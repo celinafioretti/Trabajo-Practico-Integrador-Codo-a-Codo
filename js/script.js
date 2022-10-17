ab05cbf74f40def8bc3aa993e8773386274c1368
Celina Fioretti <celinainesfioretti@gmail.com>
2022-10-17 10:07:27 -0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,18 +1,12 @@
-// VALOR DEL TICKET $200    
+// Seleccion de Elementos y Declaracion de variables
 let valorTicket = 200;
-
 let cantidadForm = document.getElementById('cantidad-form');
-
 let seleccionCategoria = document.getElementById('selector-categoria');
-
 let totalPagar = document.getElementById('total-pagar');
-
 let botonBorrar = document.getElementById('botonBorrar');
-
 let botonResumen = document.getElementById('botonResumen');
 
-
-
+// Funciones
 function valorTotalTicket () {
     if (seleccionCategoria.value == 'Estudiante') {
         valorTicket -= valorTicket * 80 / 100;
@@ -27,15 +21,17 @@ function valorTotalTicket () {
 }
 
 function editarTotalAPagar () {
-    totalPagar.textContent = `Total a Pagar: ${valorTotalTicket()}`;
-}
+    if (isNaN(cantidadForm.value)) {
+        totalPagar.textContent = `Total a Pagar: Debe colocar un Número`; 
+    }
+    else {
+        totalPagar.textContent = `Total a Pagar: ${valorTotalTicket()}`;
+        valorTicket = 200;
+    }
 
-function editarValorTotal () {
-    valorTicket = 200;
-    totalPagar.textContent = `Total a Pagar: ${0}`;
 }
+let borrarTotal = () => totalPagar.textContent = `Total a Pagar: ${0}`;
 
+// Eventos
 botonResumen.addEventListener('click',  editarTotalAPagar);
-botonBorrar.addEventListener('click', editarValorTotal);
-
-//TODO: ver si se puede lograr que el resultado no descuente una y otra vez el valorTotalAPagar
+botonBorrar.addEventListener('click', borrarTotal);
